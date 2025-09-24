5affd4b3073311d49705ea332f2a7de4ee1765e1
jinhyuk9714 <jinhyuk9714@gmail.com>
2025-09-25 03:07:36 +0900

    Initial commit

---
+++ src/main/java/com/example/embed_chatbot/EmbedChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.embed_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbedChatbotApplication.class, args);
+	}
+
+}
